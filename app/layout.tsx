185ff36f2d55c792c6f7fc38d6845149134f97b8
Gabriel Strobel <gabrielcoelho91@gmail.com>
2024-01-09 01:51:16 -0300

    fix: remove hotjar code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Sora } from 'next/font/google'
 import './globals.css'
 import { GoogleTagManager } from '@next/third-parties/google'
-import Hotjar from '@hotjar/browser';
 
 const sora = Sora({ subsets: ['latin'] })
 
@@ -16,7 +15,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  Hotjar.init(3818201, 6);
   return (
     <html lang="en">
       <GoogleTagManager gtmId="GTM-NDB7Z5J6" />
